c0587ffc43199624049b64a3a58e37fd68c96f30
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-01-24 09:55:24 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-# sets up web server , preparing web server for deployment
+# Prepare your web servers
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo useradd ubuntu
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo chown -R $USER:$USER /data
+sudo echo "<h1>Hello ALX!</h1>" > /data/web_static/releases/test/index.html
+
+addLink="\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo sed -i "25i\ $addLink" /etc/nginx/sites-available/default
+
 sudo service nginx restart
 sudo service nginx reload
