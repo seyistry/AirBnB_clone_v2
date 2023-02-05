c8f601decba83695b554f4b293fa1ba33d2ed078
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-05 22:40:45 +0100

    ex 7

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web
+application. Is it a number!
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route("/")
+def print_hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def print_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def print_c_fun(text):
+    value = text.replace("_", " ")
+    return "C %s" % value
+
+
+@app.route('/python')
+@app.route("/python/<text>")
+def print_python_fun(text='is cool'):
+    value = text.replace("_", " ")
+    return "Python %s" % value
+
+
+@app.route("/number/<int:n>")
+def print_check_num(n):
+    if isinstance(n, int):
+        return "%d is a number" % n
+
+
+@app.route("/number_template/<int:n>")
+def print_num_template(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def print_odd_even(n):
+    if isinstance(n, int):
+        return render_template("6-number_odd_or_even.html", num=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
