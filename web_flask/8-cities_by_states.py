d92ef0700423935a3c963226afe3f27e4a0042e2
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-06 11:53:44 +0100

    test fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,7 @@ List of states
 from models import storage
 from models.state import State
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,7 +14,7 @@ app.jinja_env.lstrip_blocks = True
 
 
 @app.route('/cities_by_states')
-def run_all_states():
+def run_all_states_cites():
     """Run all states"""
     l_list = storage.all(State)
     return render_template('8-cities_by_states.html', l_list=l_list)
